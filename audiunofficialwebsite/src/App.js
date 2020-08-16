f89f3dc85f2544ae8fc3c131fdc81f554bd78494
kobrata0912 <kobrata0912@gmail.com>
2020-08-16 18:29:02 +0300

    Created topbar and navigation

--- audiunofficialwebsite/src/App.js
+++ audiunofficialwebsite/src/App.js
@@ -1,9 +1,15 @@
-
+import React from 'react'
+import Header from './components/header'
+import Navigation from './components/navigation'
 import './App.css';
 
 function App() {
   return (
-
+    <React.Fragment>
+    <Header />
+    <Navigation />
+    </React.Fragment>
+  )
 }
 
 export default App;
